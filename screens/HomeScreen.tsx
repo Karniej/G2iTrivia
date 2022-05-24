9b6218ae46476eea937bdad14e50594e8ff97190
Paweł Karniej <karniej.p@gmail.com>
2022-05-24 13:43:36 +0200

    added axios & HomeScreen navigation

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,18 @@
-import { StyleSheet } from 'react-native'
+import { Button, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { Text, View } from '../components/Themed'
 import { RootStackScreenProps } from '../types'
 
 export default function HomeScreen({ navigation }: RootStackScreenProps<'Home'>) {
+  const navigateToQuiz = () => navigation.navigate('Quiz')
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>HomeScreen</Text>
-      <View style={styles.separator} lightColor='#eee' darkColor='rgba(255,255,255,0.1)' />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Welcome to the Trivia Challenge</Text>
+      <Text style={styles.subtitle}>You will be presented with 10 True or False questions</Text>
+      <Text style={styles.subtitle}>Can you score 100%?</Text>
+      <Button color='black' title='BEGIN' onPress={navigateToQuiz} />
+    </SafeAreaView>
   )
 }
 
@@ -16,15 +20,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 50,
+    paddingVertical: 5,
   },
   title: {
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  subtitle: {
+    fontSize: 25,
+    paddingHorizontal: 25,
+    textAlign: 'center',
   },
 })
